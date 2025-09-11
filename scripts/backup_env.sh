24a771d99b3979ef40a28985ef44c610265dac1c
liuyx <yuxuan906@foxmail.com>
2025-09-11 11:29:41 +0800

    backup 2025-09-11 11:29:41

---
+++ scripts/backup_env.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+env_repo=~/develop-env
+
+echo "[1/3] Copy config and script files."
+cp ~/.vimrc ${env_repo}
+cp ~/.zshrc ${env_repo}
+cp ~/.bashrc ${env_repo}
+cp ~/.gitconfig ${env_repo}
+cp ~/scripts/* ${env_repo}/scripts
+cp ${LINUX_ROOT}/*.sh ${env_repo}/linux
+
+# backup .config files
+echo "[2/3] Copy linux .config files."
+find "${LINUX_BUILD}" -name ".config" -type f | while read config_file; do
+	rel_path="${config_file#$LINUX_BUILD/}"
+	dest_dir="${env_repo}/linux/linux-build/$(dirname "$rel_path")"
+	dest_file="$dest_dir/.config"
+
+	mkdir -p "$dest_dir"
+	cp "$config_file" "$dest_file"
+done
+
+echo "[3/3] Commit and push to remote."
+pushd ${env_repo}
+if git status --porcelain | grep -q '^[ MADRCU?][ MDAU?TRC ]'; then
+	COMMIT_MSG="backup $(date '+%Y-%m-%d %H:%M:%S')"
+
+	git add -A .
+	git commit -m "$COMMIT_MSG"
+#	git push origin
+fi
+popd
